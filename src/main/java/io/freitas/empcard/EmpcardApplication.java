3e48efd5aedc72e141dbccbac4324b11bbdbbc48
Daniel Freitas <danielsfreitas97@gmail.com>
2026-02-08 10:46:49 -0300

    Commit inicial

---
+++ src/main/java/io/freitas/empcard/EmpcardApplication.java
@@ -0,0 +1,13 @@
+package io.freitas.empcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpcardApplication.class, args);
+	}
+
+}
